0d1574bbc20ae62a2c90bfef1b20f26f4225e4b6
Christina2208 <christinalf22@gmail.com>
2023-07-15 21:16:03 -0400

    apiRoutes code

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+//importing modules
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+// path defined to the db.json file
+const dbPath = path.join(__dirname, '..', 'db', 'db.json');
+
+function Notes() {
+    return JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+}
+
+// saves to the db.json file
+function noteSaving(notes) {
+    fs.writeFileSync(dbPath, JSON.stringify(notes));
+}
+
+router.get('/notes', (req, res) => {
+    const notes = Notes();
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = Notes();
+    newNote.id = Date.now().toString();
+    notes.push(newNote);
+    noteSaving(notes);
+    res.json(newNote);
+});
+
+module.exports = router;
